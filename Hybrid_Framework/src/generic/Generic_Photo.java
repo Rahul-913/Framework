6e2728efe24e601c8599c4dd5d63cc90f8014c67
s <s@DESKTOP-AHGNOB7>
2024-03-14 10:58:25 +0530

    Updated

--- Hybrid_Framework/src/generic/Generic_Photo.java
+++ Hybrid_Framework/src/generic/Generic_Photo.java
@@ -12,7 +12,7 @@ public class Generic_Photo
 {
 	public static void getPhoto(WebDriver driver) throws IOException
 	{
-		System.out.println("Started");
+		System.out.println("execution Started");
 		String photo = "C:\\Users\\Faculty\\Desktop\\Selenium\\Screenshots";
 		Date d = new Date();
 		String d1 = d.toString();
