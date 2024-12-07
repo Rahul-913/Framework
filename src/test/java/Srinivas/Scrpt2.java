892824ca9ffdf6f8b3258c63d121f84d0de8d83a
Srinivas <Srini8192@gmail.com>
2024-12-07 08:30:10 +0530

    Commit for 2nd time

---
+++ src/test/java/Srinivas/Scrpt2.java
@@ -0,0 +1,13 @@
+package Srinivas;
+
+import org.testng.annotations.Test;
+
+public class Scrpt2 {
+	@Test 
+	public void Case()
+	{
+		System.out.println("Mission Accomplished");
+		System.out.println("Mission Accomplished");
+	}
+
+}
