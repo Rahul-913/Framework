3b2d64969edf488fad0143d8f319cf4587d2b3a9
Srinivas <Srini8192@gmail.com>
2024-12-07 08:18:27 +0530

    testcase from srinivas

---
+++ src/test/java/Srinivas/Scp01.java
@@ -0,0 +1,16 @@
+package Srinivas;
+
+import org.testng.annotations.Test;
+
+public class Scp01 {
+ 
+
+	@Test 
+	public void Case()
+	{
+		System.out.println("Mission Accomplished");
+		System.out.println("Mission Accomplished");
+	}
+	
+	
+}
