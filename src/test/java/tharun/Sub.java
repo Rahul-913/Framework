222b8212cad111f0aa6a6ec44fc34bb50a43f1d3
Tester <Tester@gmail.com>
2024-12-07 08:16:58 +0530

    Testcase from Tharun

---
+++ src/test/java/tharun/Sub.java
@@ -0,0 +1,16 @@
+package tharun;
+
+import org.testng.annotations.Test;
+
+public class Sub {
+
+	public class Add {
+		 
+		
+		@Test
+		public void add() {
+			System.out.println("Print statement in add class");
+		}
+
+	}
+}
